60fefd27aefb0a16d6ccd2b2b2138177b6ca7721
azrinurvani <azrinurvani@gmail.com>
2020-09-02 08:14:11 +0700

    first commit

---
+++ SimpleCRUDForBeginner/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace SimpleCRUDForBeginner
+{
+    partial class FormStudent
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.genderTextBox = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.AllowUserToAddRows = false;
+            this.dataGridViewStudent.AllowUserToDeleteRows = false;
+            this.dataGridViewStudent.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(296, 0);
+            this.dataGridViewStudent.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewStudent.MultiSelect = false;
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.ReadOnly = true;
+            this.dataGridViewStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewStudent.Size = new System.Drawing.Size(556, 296);
+            this.dataGridViewStudent.TabIndex = 0;
+            this.dataGridViewStudent.Text = "dataGridView1";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 19);
+            this.label2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(72, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(146, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Welcome Student";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(7, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "First Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(7, 70);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(7, 104);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(7, 140);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Password";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(7, 179);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Gender";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.firstNameTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.firstNameTextBox.Location = new System.Drawing.Point(84, 37);
+            this.firstNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(191, 25);
+            this.firstNameTextBox.TabIndex = 8;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lastNameTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lastNameTextBox.Location = new System.Drawing.Point(84, 67);
+            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(191, 25);
+            this.lastNameTextBox.TabIndex = 9;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.emailTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailTextBox.Location = new System.Drawing.Point(84, 100);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(191, 25);
+            this.emailTextBox.TabIndex = 10;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.passwordTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordTextBox.Location = new System.Drawing.Point(84, 136);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(191, 25);
+            this.passwordTextBox.TabIndex = 11;
+            // 
+            // genderTextBox
+            // 
+            this.genderTextBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.genderTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.genderTextBox.Location = new System.Drawing.Point(84, 175);
+            this.genderTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.genderTextBox.Name = "genderTextBox";
+            this.genderTextBox.Size = new System.Drawing.Size(191, 25);
+            this.genderTextBox.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(215, 206);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(58, 29);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(663, 303);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(71, 31);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(385, 303);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(77, 31);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(141, 206);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 31);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(524, 303);
+            this.btnShow.Margin = new System.Windows.Forms.Padding(2);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(82, 32);
+            this.btnShow.TabIndex = 17;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // FormStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 346);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.genderTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewStudent);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "FormStudent";
+            this.Text = "Form Student";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox genderTextBox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ SimpleCRUDForBeginner/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+namespace SimpleCRUDForBeginner
+{
+    public partial class FormStudent : Form
+    {
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataAdapter adapter;
+        DataSet dataSet = new DataSet();
+        DataTable dataTable = new DataTable();
+        String connectString;
+
+        int id;
+        bool isDoubleClick = false;
+        public FormStudent()
+        {
+            InitializeComponent();
+            connectString = @"Data source=D:\Azri's Project\.NET Core\database\net_db.db;version=3";
+            RetrieveData();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            RetrieveData();
+        }
+
+        private void RetrieveData()
+        {
+            try
+            {
+                conn = new SQLiteConnection(connectString);
+                conn.Open();
+                String query = "SELECT * FROM Student";
+                adapter = new SQLiteDataAdapter(query, conn);
+                dataSet.Reset();
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+                dataGridViewStudent.DataSource = dataTable;
+                conn.Close();
+                dataGridViewStudent.Columns[0].HeaderText = "id";
+                dataGridViewStudent.Columns[1].HeaderText = "First Name";
+                dataGridViewStudent.Columns[2].HeaderText = "Last Name";
+                dataGridViewStudent.Columns[3].HeaderText = "Email";
+                dataGridViewStudent.Columns[4].HeaderText = "Password";
+                dataGridViewStudent.Columns[5].HeaderText = "Gender";
+                dataGridViewStudent.Columns[0].Visible = false;
+                dataGridViewStudent.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridViewStudent.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                dataGridViewStudent.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ClearData()
+        {
+            id = 0;
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            emailTextBox.Text = "";
+            passwordTextBox.Text = "";
+            genderTextBox.Text = "";
+            dataGridViewStudent.ClearSelection();
+            dataGridViewStudent.CurrentCell = null;
+            isDoubleClick = false;
+        }
+
+
+        private void GetIdStudent()
+        {
+            //id = Convert.ToInt32(dataGridViewStudent.SelectedRows[0].Cells[0].Value);
+         
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (firstNameTextBox.Text != "" || lastNameTextBox.Text != "" || emailTextBox.Text != ""
+                || passwordTextBox.Text != "" || genderTextBox.Text != "")
+            {
+
+                try
+                {
+                    conn = new SQLiteConnection(connectString);
+                    cmd = new SQLiteCommand();
+                    cmd.CommandText = @"INSERT INTO Student(first_name,last_name,email,password,gender) 
+                               VALUES(@fname,@lname,@email,@password,@gender)";
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add(new SQLiteParameter("@fname", firstNameTextBox.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@lname", lastNameTextBox.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@email", emailTextBox.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@password", passwordTextBox.Text));
+                    cmd.Parameters.Add(new SQLiteParameter("@gender", genderTextBox.Text));
+
+                    conn.Open();
+
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Successfully Created!");
+                        firstNameTextBox.Text = "";
+                        lastNameTextBox.Text = "";
+                        emailTextBox.Text = "";
+                        passwordTextBox.Text = "";
+                        genderTextBox.Text = "";
+                        RetrieveData();
+                        conn.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Required Failed");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(dataGridViewStudent.SelectedRows[0].Cells[0].Value);
+            DialogResult dialogResult =
+                MessageBox.Show("Do you want to delete this record ? ",
+                "Warning",MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+
+            cmd = new SQLiteCommand();
+            if(dialogResult == DialogResult.Yes)
+            {
+                try
+                {
+                    conn = new SQLiteConnection(connectString);
+                    conn.Open();
+                    cmd.CommandText = @"DELETE FROM Student WHERE ID = '" + id + "'";
+                    cmd.Connection = conn;
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        id = 0;
+                        dataGridViewStudent.ClearSelection();
+                        dataGridViewStudent.CurrentCell = null;
+                        RetrieveData();
+                        dataGridViewStudent.ClearSelection();
+                        dataGridViewStudent.CurrentCell = null;
+                        MessageBox.Show("Successfully delted!");
+                        isDoubleClick = false;
+
+                    }
+                    conn.Close();   
+                }catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if(dialogResult == DialogResult.No)
+            {
+                
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            //if (isDoubleClick)
+            //{
+                try
+                {
+                    id = Convert.ToInt32(dataGridViewStudent.SelectedRows[0].Cells[0].Value);
+                    conn.Open();
+                    cmd = new SQLiteCommand();
+                    cmd.CommandText = @"UPDATE Student SET first_name=@fname,last_name=@lname,
+                                    email=@email, password=@password,gender=@gender
+                                    WHERE ID = '" + id + "'";
+                    cmd.Connection = conn;
+                    cmd.Parameters.AddWithValue("@fname", firstNameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@lname", lastNameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@email", emailTextBox.Text);
+                    cmd.Parameters.AddWithValue("@password", passwordTextBox.Text);
+                    cmd.Parameters.AddWithValue("@gender", genderTextBox.Text);
+
+                    int i = cmd.ExecuteNonQuery();
+                    if (i == 1)
+                    {
+                        MessageBox.Show("Successfully Updated");
+                        ClearData();
+                        RetrieveData();
+                        id = 0;
+                        dataGridViewStudent.ClearSelection();
+                        dataGridViewStudent.CurrentCell = null;
+                    }
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+           // }
+           
+        }
+
+        
+    }
+}
